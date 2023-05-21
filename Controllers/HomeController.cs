171fb5aa7e67927a207c3963755e000566f4975e
TD <TD@DESKTOP-5NGF4PT>
2023-05-21 03:09:54 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Online__Store_Movies.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
